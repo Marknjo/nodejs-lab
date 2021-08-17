f7d46243a16ab38cb6ae001817ddb52e8e044dfd
Marknjo <marknjo7@gmail.com>
2021-08-17 13:24:40 +0300

    Experimenting with readable.read()

--- playground/learning-streams/infinite-read.js
+++ playground/learning-streams/infinite-read.js
@@ -1,10 +1,30 @@
 import fs from 'fs';
-
-const rs = fs.createReadStream('/dev/urandom');
+console.time('chunking');
+const rs = fs.createReadStream('./file.txt', 'utf8');
 
 let size = 0;
+let chunkArr = [];
 
-rs.on('data', chunk => {
-  size += chunk.length;
-  console.log('File size: ' + size);
+rs.on('readable', () => {
+  let chunk;
+  console.log('Stream is readable (new data received in buffer)');
+  // Use a loop to make sure we read all currently available data
+
+  while (null !== (chunk = rs.read())) {
+    console.log(`Read ${chunk.length} bytes of data...`);
+    chunkArr.push(chunk);
+  }
 });
+
+// 'end' will be triggered once when there is no more data available
+rs.on('end', () => {
+  console.log('Reached end of stream.');
+  const content = chunkArr.join('');
+  //console.log(content);
+  console.timeEnd('chunking');
+});
+
+// rs.on('data', chunk => {
+//   size += chunk.length;
+//   console.log('File size: ' + size);
+// });
